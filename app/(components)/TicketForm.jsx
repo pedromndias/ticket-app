df44eef18c9d3ec99c28afb2b92e068b753ca083
Pedro Dias <pedrodias@Pedros-MacBook-Pro-3.local>
2023-10-29 17:48:44 +0100

    Create ticket form

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,161 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const TicketForm = () => {
+  // State managed form.
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    e.preventDefault()
+    const res = await fetch("api/Tickets", {
+        method: "POST",
+        body: JSON.stringify({formData}),
+        "content-type": "application/json"
+    })
+
+    if (!res.ok) {
+        throw new Error("Failed to create Ticket.")
+    }
+  };
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "Not started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
+
+  return (
+    <div className="flex justify-center">
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>Create your ticket</h3>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows="5"
+        />
+
+        <label htmlFor="category">Category</label>
+        <select
+          name="category"
+          id="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Hardware Problem">Hardware Problem</option>
+          <option value="Software Problem">Software Problem</option>
+          <option value="Project">Project</option>
+        </select>
+
+        <label>Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label htmlFor="priority-1">1</label>
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label htmlFor="priority-2">2</label>
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label htmlFor="priority-3">3</label>
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label htmlFor="priority-4">4</label>
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label htmlFor="priority-5">5</label>
+        </div>
+
+        <label htmlFor="progress">Progress</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          value={formData.progress}
+          min="0"
+          max="100"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="status">Status</label>
+        <select
+          name="status"
+          id="status"
+          value={formData.status}
+          onChange={handleChange}
+        >
+            <option value="not started">Not started</option>
+            <option value="started">Started</option>
+            <option value="done">Done</option>
+        </select>
+
+        <input type="submit" className="btn max-w-xs" value="Create Ticket"/>
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
